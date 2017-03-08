0cc31f62cfd411e70bec8190d362bfeda76e63fd
Nagalalithadevi <Nagalalithadevi.Guduru@cognizant.com>
2017-03-08 15:01:40 +0530

    Update MyUser.java

--- src/main/java/com/jverstry/Data/MyUser.java
+++ src/main/java/com/jverstry/Data/MyUser.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class MyUser {
+public  MyUser {
 	
 	@NotNull
 	@Size(min=1,max=20)
